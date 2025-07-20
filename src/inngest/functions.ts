e7f97ae47c5e6e4cc09f791ac9bde6b557e8acfe
JHoo0118 <kik3078@gmail.com>
2025-07-20 18:41:25 +0900

    feat: AI job

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,18 +1,22 @@
+import { createAgent, openai } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    // Imagine this is a long download step
-    await step.sleep("wait-a-moment", "30s");
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert Next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
+      model: openai({ model: "gpt-4o-mini" }),
+    });
 
-    // Imagine this is a long transcript step
-    await step.sleep("wait-a-moment", "10s");
+    const { output } = await codeAgent.run(
+      `Write a following snippet: ${event.data.value}`
+    );
 
-    // Imagine this is a summary step
-    await step.sleep("wait-a-moment", "5s");
-
-    return { message: `Hello ${event.data.email}!` };
+    return { output };
   }
 );
